ef311671f63538384af8806d6f9d83429529cfa2
shadowplahue <183840614@qq.com>
2022-09-11 22:34:02 +0800

    最后一个单词长度

---
+++ 最后一个单词长度.java
@@ -0,0 +1,26 @@
+package jax;
+
+public class 最后一个单词长度 {
+	public int lengthOfLastWord(String s) {
+
+        int record=0;
+        int res=0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)==' '){
+             record=0;
+            
+                if(record==0){
+                  
+                    continue;}
+                }  
+        
+            else{
+                record++;
+    
+                res=record;
+            }
+        }
+        return res;
+    }
+}
+
